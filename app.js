01facd973a86a37ca7aa93a7c3e2ee9b66e60a59
Yeswanth <gajjalayeswanth@gmail.com>
2022-04-07 18:54:49 +0530

    updated path to models

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const Pitch = require('./models/Pitchscema');
+const Pitch = require('./models/Pitchscema.js');
 mongoose.connect("mongodb://localhost:27017/xharktank", {useNewUrlParser : true});
 
 
