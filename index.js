1b11e92028d3054df7f41c67bf0cd34e24159690
Yeswanth <gajjalayeswanth@gmail.com>
2022-04-08 21:34:22 +0530

    Changed the printing order of pitches

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Pitch = require( __dirname + '/models/PitchScema.js');
 
-mongoose.connect("mongodb://localhost:27017/xharktank", {useNewUrlParser : true});
+mongoose.connect("mongodb://localhost:27017/xharktank3", {useNewUrlParser : true});
 
 
 const app = express();
@@ -16,7 +16,7 @@ let a = [1]
 
 let id = String(a.length);
 
-app.post('/pitches',  (req, res) => {
+app.post('/pitches', async (req, res) => {
     const reqresult = req.body;
     const data = new Pitch({
         id : id,
@@ -29,7 +29,7 @@ app.post('/pitches',  (req, res) => {
     if(data.equity > 100 || data.askAmount < 0){
         return res.status(400).json();
     }
-     data.save(function(err, dat){
+    await data.save(function(err, dat){
         if(err){return res.status(400).json()}
         else{
             a.push(1);
@@ -73,7 +73,11 @@ app.get('/pitches', async (req, res) => {
 
     await Pitch.find({}, {'_id': 0})
     .then(user => {
-        return res.status(200).json(user);
+        let dat = user;
+        for(let i = 0; i < (dat.length)/2; i++){
+            [dat[i], dat[dat.length - i-1]] = [dat[dat.length-i-1], dat[i]];
+        }
+        return res.status(200).json(dat);
     })
     .catch(err => console.log(err));
 })
